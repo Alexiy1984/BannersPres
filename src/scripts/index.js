b4ae99f197ae89d3bc92b95a7955779510d862ec
Alex <alevaliev@yandex.ru>
2021-01-26 17:16:51 +0300

    added bannres, features, sizez, footer blocks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,24 @@
 import '../styles/index.scss';
+import 'bootstrap';
+import $ from "jquery";
+import LazyLoad from 'vanilla-lazyload';
+import WOW from 'wow.js';
+
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.pug');
 }
+
+$('.sizes-box__button').on('click', function() {
+  var dataTarget = $('#' + $(this).attr('data-target'));
+
+  if (dataTarget.length) {
+    $('.banners-show__row').hide();
+    console.log(dataTarget[0]);
+    dataTarget.show();
+    $('.sizes-box__button').removeClass('active');
+    $(this).addClass('active');
+  }
+});
 
 console.log('webpack starterkit');

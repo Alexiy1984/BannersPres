3aeeb309200902425c4aa9b5b75fe5aec04b92ef
Alex <alevaliev@yandex.ru>
2021-01-27 17:38:33 +0300

    added most of bloacks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,21 +4,39 @@ import $ from "jquery";
 import LazyLoad from 'vanilla-lazyload';
 import WOW from 'wow.js';
 
-
 if (process.env.NODE_ENV === 'development') {
   require('../index.pug');
 }
 
-$('.sizes-box__button').on('click', function() {
-  var dataTarget = $('#' + $(this).attr('data-target'));
+$(function() {
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: '.lazy',
+  });
 
-  if (dataTarget.length) {
-    $('.banners-show__row').hide();
-    console.log(dataTarget[0]);
-    dataTarget.show();
-    $('.sizes-box__button').removeClass('active');
-    $(this).addClass('active');
-  }
+  var wow = new WOW({
+    boxClass:     'wow',      
+    animateClass: 'animate__animated',     
+    mobile:       true,       
+    live:         true,       
+    scrollContainer: null,    
+    resetAnimation: true, 
+  });
+
+  wow.init();
+
+  $('.sizes-box__button').on('click', function() {
+    var dataTarget = $('#' + $(this).attr('data-target'));
+  
+    if (dataTarget.length) {
+      $('.banners-show__row').hide();
+      console.log(dataTarget[0]);
+      dataTarget.show();
+      $('.sizes-box__button').removeClass('active');
+      $(this).addClass('active');
+    }
+  });
 });
+
+
 
 console.log('webpack starterkit');
